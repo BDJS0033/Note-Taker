200d09b2dfd51622295d7703fd3ff4cfaf328b7d
SAB <sevanboyadjian@gmail.com>
2022-03-15 10:10:13 -0400

    setting up code

---
+++ server.js
@@ -0,0 +1,21 @@
+//Noting packages
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+//middleware for parsing/json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+//starting server
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
