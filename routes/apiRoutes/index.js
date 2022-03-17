bd2a9a6d0b7864507e0f5149a574d738b2b185e4
SAB <sevanboyadjian@gmail.com>
2022-03-16 20:01:54 -0400

    creating get and post functions

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,14 +1,51 @@
 //calling packages
 const fs = require("fs");
-const router = require('express').Router();
-const path = require('path');
+const path = require("path");
 
-//Generates random id
-const { v4: uuidv4 } = require('uuid');
+// // Generates random id - not reading it
+// const { v4: uuidv4 } = require("uuidv4");
 
-//Notes Database
-const notesDB = require('../../db/db.json');
+//Create exporting function - maintain/manage code
+//utf-8 to prevent buffer
+module.exports = app => {
 
+    fs.readFile("../../db/db.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        const notesDB = JSON.parse(data);
 
+        //Creating GET & POST route for notesBD
+        // creating get and post route for notes input
 
-module.exports = router;
+        app.get("/api/notes", function (req, res) {
+            //return as json
+            res.json(notesDB);
+        });
+
+        //POST request to add new notes
+        app.post("/api/notes", function (req, res) {
+            let newNote = req.body;
+            notesDB.push(newNote);
+            updateDb();
+            return console.log("Add Note: " + newNote.title);
+        });
+
+        //Creating GET & Delete Request for notes
+        app.get("/api/notes/:id", function (req, res) {
+            res.json(notesDB[req.params.id]);
+        });
+
+        app.delete("/api/notes/:id", function (req, res) {
+            notesDB.splice(req.params.id, 1);
+            updateDb();
+            console.log("Note has been deleted, ID: " + req.params.id);
+        });
+
+        //Update db.json when notes added and deleted
+        function updateDb() {
+            fs.writeFile("../../db/db.json", JSON.stringify(notesDB, '\t'), err => {
+                if (err) throw err;
+                return true;
+            });
+        }
+
+    })};
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 //Noting packages
-const express = require('express');
-const path = require('path');
-//Generates random id
-const { v4: uuidv4 } = require("uuid");
+const express = require("express");
+const path = require("path");
 
+// // //Generates random id - not reading it
+// const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 //middleware for parsing/json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// Use apiRoutes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+//Use apiRoutes
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 //starting server
 app.listen(PORT, () => {
